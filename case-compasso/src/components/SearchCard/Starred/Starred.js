d98deaa70a09600ba70d830d8df031767f286423
Bruna Nunciato <79425709+bruxxi@users.noreply.github.com>
2021-10-19 11:30:01 -0300

    remolding2.0

--- case-compasso/src/components/SearchCard/Starred/Starred.js
+++ case-compasso/src/components/SearchCard/Starred/Starred.js
@@ -5,13 +5,6 @@ const Starred = (props) => {
         return (
           <>
             <a href={repos.owner.url}>{repos.url}<h4>{repos.name.toUpperCase()}</h4></a>
-            {/* <p>{repos.full_name}</p>
-            <p>{repos.owner.login}</p>
-            <img src={repos.avatar_url} alt={repos.name}></img>
-            
-            <p>Description: {repos.description}</p>
-            <p>Language: {repos.language}</p>
-            <p>Forks: {repos.forks}</p> */}
           </>
         );
       });
